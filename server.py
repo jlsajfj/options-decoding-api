c6d3ee56b29e2cf60f14c401e0e1f62b8eac5042
Joseph Ma <jlsajfj@hotmail.com>
2022-11-16 09:39:31 -0500

    starting main logic

--- server.py
+++ server.py
@@ -7,5 +7,18 @@ app = flask.Flask(__name__)
 def health_check():
     return 'alive', 200
 
+@app.route('/', methods=['GET'])
+def get_home():
+    return 'this page is under construction, instructions will go here', 503
+
+@app.route('/', methods=['POST'])
+def crawler_server():
+    f_body = flask.request.json
+    
+    if 'option' not in f_body:
+        return 'encoded option missing in request', 400
+    
+    return f_body, 200
+
 if __name__ == '__main__':
     app.run()
